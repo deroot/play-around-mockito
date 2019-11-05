dda95c5342e0658de9068e1e0db62e52be771637
Narupon Wannaram <narupon.wa@gmail.com>
2019-11-06 04:42:26 +0700

    first commit

---
+++ src/main/java/com/example/playaroundmockito/PlayAroundMockitoApplication.java
@@ -0,0 +1,13 @@
+package com.example.playaroundmockito;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlayAroundMockitoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlayAroundMockitoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/playaroundmockito/PlayAroundMockitoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.playaroundmockito;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PlayAroundMockitoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
